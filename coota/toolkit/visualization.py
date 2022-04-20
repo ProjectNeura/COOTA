908888bdf1ee7a947fc972f36a1fd611214b750e
ATATC <futerry@outlook.com>
2022-04-20 10:05:39 +0800

    Visualization improved.

--- coota/toolkit/visualization.py
+++ coota/toolkit/visualization.py
@@ -11,13 +11,39 @@ def show(obj) -> None:
 
 
 @show.register(_g.Chooser)
-def _(obj: _g.Chooser) -> None:
+def _(obj: _g.Chooser, title: str = "") -> None:
     d = obj.choices(range(100), 100)
     _plt.hist(d)
+    _plt.xlabel("Item")
+    _plt.ylabel("Possibility")
+    _plt.title(title)
     _plt.show()
+
+
+@show.register(_g.Generator)
+def _(obj: _g.Generator) -> None:
+    print("[%s] {" % str(type(obj))[8:-2])
+    print(f"....Arguments -> {obj.get_args()}")
+    print(f"....Default Arguments -> {obj.get_default_args()}")
+    print(f"....Parseable -> {obj.get_parseable()}")
+    print(f"....Use Cache -> {obj.get_uc()}")
+    print("}")
+
+
+@show.register(_g.GeneratorOutput)
+def _(obj: _g.GeneratorOutput) -> None:
+    show(obj.get_output())
 
 
 @show.register(list)
-def _(obj: list) -> None:
+def _(obj: list, title: str = "") -> None:
     _plt.hist(obj)
+    _plt.xlabel("Item")
+    _plt.ylabel("Count")
+    _plt.title(title)
     _plt.show()
+
+
+@show.register(str)
+def _(obj: str) -> None:
+    print(obj)
